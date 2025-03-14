56846ff1330e6484ae85c6c78560bf89e0e73e05
helpstablook <hellenjunqueira12@gmail.com>
2025-03-14 23:44:15 +0000

    14-03-25

--- Modulo II/Programação Web II/Dogdev/assets/js/script.js
+++ Modulo II/Programação Web II/Dogdev/assets/js/script.js
@@ -1,21 +1,21 @@
-//Menu HEADER
-
-//Seleciona o menuOper
-let menuOper = document.querySelector('.menu-opener');
-
-//Seleciona o nav dentro do header
+//menu HEADER
+//seleciona o menuOpener
+let menuOpener = document.querySelector('.menu-opener');
+//seleciona o nav dentro do header
 let nav = document.querySelector('header nav');
-
-//Cria um listener para o evento click do menuOpener
-menuOper.addEventListener('click', ()=>{
+//cria um listener para o evento click no menuOpener
+menuOpener.addEventListener('click', ()=>{
     if(nav.classList.contains('opened')){
         nav.classList.remove('opened');
-        menuOper.querySelector('.close-icon').style.display='none';
-        menuOper.querySelector('.hamburguer-icon').style.display='flex';
-
-     }else{
+        menuOpener.querySelector('.close-icon').style 
+            .display='none';
+        menuOpener.querySelector('.hamburger-icon').style
+            .display='flex';
+    }else{
         nav.classList.add('opened');
-        menuOper.querySelector('.close-icon').style.display='flex';
-        menuOper.querySelector('.hamburguer-icon').style.display='none';
+        menuOpener.querySelector('.close-icon').style 
+        .display='flex';
+        menuOpener.querySelector('.hamburger-icon').style
+        .display='none';
     }
 });
